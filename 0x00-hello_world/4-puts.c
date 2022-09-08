412e2221944f482285360433b2a56eb73d8921dd
Bolakale <bolakalebb@gmail.com>
2022-09-08 13:14:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,16 +3,11 @@
 
 
 /**
+ * main - Entry point  
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
+ *    Return: Always 0 (Success)
+ */
+ int main(void)
 
 {
 
