c6420eb7dea6925ec186ef08d27ef7b2c9e9b284
Bolakale <bolakalebb@gmail.com>
2022-09-11 14:20:09 -0700

    combs 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,9 @@
 	{
 		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
+			if (digit2 != digit1)
+			{
+				putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
@@ -21,6 +23,8 @@
 
 			putchar(',');
 			putchar(' ');
+
+			}
 		}
 	}
 
