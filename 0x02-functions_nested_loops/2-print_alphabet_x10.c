1bdc590157abede438dbe52f5929fb447d010eb8
Bolakale <bolakalebb@gmail.com>
2022-09-13 14:48:49 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (lettter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			-putchar(letter);
 		_putchar('\n')
 	}
 }
