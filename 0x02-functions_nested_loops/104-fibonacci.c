e6098b6d41a5ddc5d4b2fac6e00a42e72bb49391
Bolakale <bolakalebb@gmail.com>
2022-09-13 23:45:58 -0700

    fibs

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,8 +24,8 @@ int main(void)
 	}
 
 	fib1_half1 = fib1 / 10000000000;
-	fib1_half1 = fib1 / 10000000000;
-	fib1_half1 = fib1 % 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 
 	for (count = 93; count < 99; count++)
@@ -40,15 +40,13 @@ int main(void)
 
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
-			printf(",");
-
-				fib1_half1 = fib2_half1;
-		fib1_half1 = fib2_half1;
+			printf(", ");
+		fib1_half1 = fib2_half1
+		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
 	printf("\n");
 	return (0);
 }
-
 	
