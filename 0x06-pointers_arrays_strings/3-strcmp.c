2d0548999a50ea7a9fb1e55752f2261c75e4eb62
Bolakale <bolakalebb@gmail.com>
2022-09-22 04:36:10 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  *  @s1: first string
  *  @s2: second string.
  *  
- *  Return: If str1 < str2, the negative difference of
- *  the first unmatched characters.
- *  If str1 == str2, 0.
+ *  Return: If str1 < str2, the negative difference of the first
+ *  unmatched characters.
+ *  If str1 == str2, 0
  *  If str1 > str2, the positive difference of the first unmatched characters.
  */
 
