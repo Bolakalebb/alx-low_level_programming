eed78156cb772285d75eb4020c903ec835a69f37
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-12 23:23:03 +0100

    0-main

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
  * main - Entry point
  *
  * Return: Always EXIT_SUCCESS
-*/
-int linear_search(int *array, size_t size, int value);
-
+ */
 int main(void)
 {
-	int arr[] = { 5, 2, 7, 10, 1 };
-	int size = sizeof(arr) / sizeof(arr[0]);
-	int value = (7);
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-	int index = linear_search(arr, size, value);
-
-	if (index == -1)
-	printf("Value %d not found in the array.\n", value);
-
-	else
-
-	printf("Value %d found at index %d.\n", value, index);
-
-	return (0);
-}
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
