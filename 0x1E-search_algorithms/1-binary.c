2fd2f6785313e089150620b60a6a6d5a5dc495ab
Bolakale <bolakalebb@gmail.com>
2023-04-12 16:37:22 -0700

    binary_updated

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,28 +13,35 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int low = 0, high = size - 1;
+	int low, mid, high;
+	int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	low = 0;
+	high = size -1;
 
 	while (low <= high)
 	{
+		mid = (low + high) / 2;
+
 		printf("Searching in array: ");
-		for (int i = low; i <= high; i++)
+		for (i = low; i <= high; i++)
 			printf("%d%s", array[i], i == high ? "\n" : "' ");
 
-		int mid = low + (high - low) / 2;
-
-		if (array[mid] == value)
-		{
-			return (mid);
-		}
-		else if (array[mid] < value)
-		{
+		if (array[mid] < value)
 			low = mid + 1;
-		}
-		else
-		{
+		else if (array[mid] > value)
+
 			high = mid - 1;
-		}
+
+		else
+
+			return (mid);
+
 	}
 
 	return (-1);
