f9b02b2b8777e446365a736085ff9da5d90e783c
Bolakale <bolakalebb@gmail.com>
2023-04-12 04:00:02 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,17 +18,13 @@
 int linear_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
-	{
-	return (-1);
-	}
+		return (-1);
 
-		for (size_t i = 0; i < size; i++)
-		{
+	for (size_t i = 0; i < size; i++)
+	{
 		printf("Comparing array[%zu] = %d to %d\n", i, array[i], value);
 		if (array[i] == value)
-		{
 			return (i);
-		}
-		}
-		return (-1);
+	}
+	return (-1);
 }
