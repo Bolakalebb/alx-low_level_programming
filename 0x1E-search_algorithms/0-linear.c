56a56917b9586391bd20923ccf3e6cee0e6e7e3f
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-12 23:25:33 +0100

    update 0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,14 +17,20 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
+	size_t i;
 
-	for (size_t i = 0; i < size; i++)
+	if (array == NULL)
 	{
-		printf("Comparing array[%lu] = %d to %d\n", i, array[i], value);
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 		if (array[i] == value)
+		{
 			return (i);
+		}
 	}
 	return (-1);
 }
